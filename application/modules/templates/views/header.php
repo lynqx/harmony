b94764be3a02d8b4e6c421d2a4347b18e522e244
Lynqx <aakinjiola@gmail.com>
2014-06-29 21:08:49 +0100

    js scripts moved to footer for faster page load

--- application/modules/templates/views/header.php
+++ application/modules/templates/views/header.php
@@ -56,53 +56,7 @@
     <link href="css/ie8.css" rel="stylesheet" type="text/css"/><![endif]-->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
     <link href="<?php echo base_url(); ?>ckeditor/contents.css"/>
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
-    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js"></script>
-    <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?key=AIzaSyDY0kkJiTPVd2U7aTOAwhc9ySH6oHxOIYM&amp;sensor=false"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/charts/excanvas.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/charts/jquery.flot.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/charts/jquery.flot.resize.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/charts/jquery.sparkline.min.js"></script>
-
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/ui/jquery.easytabs.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/ui/jquery.collapsible.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/ui/jquery.mousewheel.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/ui/prettify.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/ui/jquery.bootbox.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/ui/jquery.colorpicker.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/ui/jquery.timepicker.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/ui/jquery.jgrowl.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/ui/jquery.fancybox.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/ui/jquery.fullcalendar.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/ui/jquery.elfinder.js"></script>
-
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/uploader/plupload.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/uploader/plupload.html4.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/uploader/plupload.html5.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/uploader/jquery.plupload.queue.js"></script>
-
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/forms/jquery.uniform.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/forms/jquery.autosize.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/forms/jquery.inputlimiter.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/forms/jquery.tagsinput.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/forms/jquery.inputmask.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/forms/jquery.select2.min.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/forms/jquery.listbox.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/forms/jquery.validation.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/forms/jquery.validationEngine-en.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/forms/jquery.form.wizard.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/forms/jquery.form.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>ckeditor/ckeditor.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/plugins/tables/jquery.dataTables.min.js"></script>
-
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/files/bootstrap.min.js"></script>
-
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/files/functions.js"></script>
-
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/charts/graph.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/charts/chart1.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/charts/chart2.js"></script>
-    <script type="text/javascript" src="<?php echo base_url(); ?>js/charts/chart3.js"></script>
+    
 
 
     <!--code for date and time function  ##Lynqx 19012014-->
